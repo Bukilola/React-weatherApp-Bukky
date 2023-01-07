43b61f569e77450119a9039b305c43dad723b131
Bukilola <Arasanyin01@gmail.com>
2023-01-07 18:15:50 +0100

    Working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import SearchEngine from "./SearchEngine";
+import SearchEngine from "./Weather";
 function App() {
   return (
     <div className="App">
       <h1>My Weather App</h1>
-      SearchEngine />
+      <SearchEngine />
       <br />
       <br />
       <br />
       <br />
       <p>
         This code is{" "}
- 
+        <a
+          href="https://github.com/Bukilola/React-weatherApp-Bukky"
+          target="-blank"
+          rel="noreferrer"
+        >
+          Open-source
+        </a>
+        , created by Arasanyin Olubukola
       </p>
     </div>
   );
